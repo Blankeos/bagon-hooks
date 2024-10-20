a0b07701ba0470f6039924355362081616b848cb
Blankeos <carloantonioct@gmail.com>
2024-10-21 01:37:21 +0800

    feat: Improved use favicon hook visuals.

--- dev/components/examples/use-favicon/use-favicon.example.tsx
+++ dev/components/examples/use-favicon/use-favicon.example.tsx
@@ -2,11 +2,12 @@ import { useFavicon } from 'src';
 import { ExampleBase } from '../example-base';
 import Code from './use-favicon.code.mdx';
 
+import { IconSolidJS, IconX } from 'dev/icons';
 import { createSignal } from 'solid-js';
 import { useMDXComponents } from 'solid-jsx';
 
 export function UseFaviconExample() {
-  const [touched, setTouched] = createSignal(false);
+  const [currentIcon, setCurrentIcon] = createSignal<'solid' | 'x' | undefined>(); // Not relevant, just for visuals
 
   // OLD Implementation:
   // const [favicon, setFavicon] = createSignal('https://docs.solidjs.com/favicon.svg');
@@ -14,8 +15,14 @@ export function UseFaviconExample() {
   // IMPROVED Implementation:
   const [_favicon, setFavicon] = useFavicon();
 
-  const setXFavicon = () => setFavicon('https://x.com/favicon.ico');
-  const setSolidFavicon = () => setFavicon('https://docs.solidjs.com/favicon.svg');
+  const setXFavicon = () => {
+    setCurrentIcon('x');
+    setFavicon('https://x.com/favicon.ico');
+  };
+  const setSolidFavicon = () => {
+    setCurrentIcon('solid');
+    setFavicon('https://docs.solidjs.com/favicon.svg');
+  };
 
   // @ts-ignore
   const components: any = useMDXComponents();
@@ -26,23 +33,30 @@ export function UseFaviconExample() {
       description="Appends <link /> element to head component with given favicon url. The hook is not called during server side rendering."
       code={<Code components={components} />}
     >
-      <div class="flex h-full w-full flex-col items-center justify-center gap-3 rounded-md border p-3 py-10 text-center text-sm">
+      <div class="relative flex h-full w-full flex-col items-center justify-center gap-3 overflow-hidden rounded-md border p-3 py-10 text-center text-sm">
+        <IconSolidJS
+          class="absolute -bottom-10 -right-10 h-48 w-48 rotate-45 transition"
+          style={{ opacity: currentIcon() === 'solid' ? 1 : 0 }}
+        />
+        <IconX
+          class="absolute -bottom-10 -right-10 h-48 w-48 rotate-45 transition"
+          style={{ opacity: currentIcon() === 'x' ? 1 : 0 }}
+        />
+
         <button
           onClick={() => {
-            setTouched(true);
             setXFavicon();
           }}
-          class="rounded-md border px-2 py-1.5 transition active:scale-95"
+          class="relative rounded-md border bg-white px-2 py-1.5 transition active:scale-95"
         >
           X favicon
         </button>
 
         <button
           onClick={() => {
-            setTouched(true);
             setSolidFavicon();
           }}
-          class="rounded-md border px-2 py-1.5 transition active:scale-95"
+          class="relative rounded-md border bg-white px-2 py-1.5 transition active:scale-95"
         >
           Solid favicon
         </button>
--- dev/icons/index.ts
+++ dev/icons/index.ts
@@ -1,5 +1,8 @@
 export { default as IconCheck } from './check';
 export { default as IconCode } from './code';
 export { default as IconCopy } from './copy';
+export { default as IconEyeDropper } from './eye-dropper';
 export { default as IconGithub } from './github';
 export { default as IconLogo } from './logo';
+export { default as IconSolidJS } from './solidjs';
+export { default as IconX } from './x';
